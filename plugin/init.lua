ed56981657d27fac4231c8a6b4a46c1c9dd2ac05
karl <puttlerk@gmail.com>
2026-02-23 22:41:40 -0800

    fix displayTimer

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,6 +2,7 @@ M = {}
 
 local SEC_PER_MIN = 60
 local MIN_PER_HR  = 60
+local SEC_PER_HR	= SEC_PER_MIN * MIN_PER_HR
 
 M.startTimer = function (min)
 	return os.time() + (min * SEC_PER_MIN)
@@ -18,19 +19,30 @@ M.checkTimer = function (endTime)
 end
 
 M.convertTime = function (time)
-	local hours					= time % (SEC_PER_MIN * MIN_PER_HR)
-	local hourRemainder	= time - (hours * (SEC_PER_MIN * MIN_PER_HR))
-	local mins					= hourRemainder % SEC_PER_MIN
-	local secs					= hourRemainder - (mins * SEC_PER_MIN)
+	local hours, mins, secs
 
-	return { hours = hours, mins = mins, secs = secs}
+	if (time < SEC_PER_HR) then
+		hours = 0
+	else
+		hours = time // SEC_PER_HR
+	end
+	local hourRemainder	= time - (hours * SEC_PER_HR)
+
+	if (hourRemainder < SEC_PER_MIN) then
+		mins = 0
+	else
+		mins = hourRemainder // SEC_PER_MIN
+	end
+	secs = hourRemainder - (mins * SEC_PER_MIN)
+
+	return hours, mins, secs
 end
 
 M.displayTimer = function (endTime)
 	local timeRemaining			= M.checkTimer(endTime)
 	local hours, mins, secs = M.convertTime(timeRemaining)
 
-	local timeStr = string.format('%d:%d:%d', hours, mins, secs)
+	local timeStr = string.format('%02d:%02d:%02d', hours, mins, secs)
 	print(timeStr)
 end
 
