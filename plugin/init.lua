f1cc05ca54528793d7422245bc2ce47ffbf66c4e
karl <puttlerk@gmail.com>
2026-02-23 22:21:59 -0800

    convertTime displayTimer

--- plugin/init.lua
+++ plugin/init.lua
@@ -17,4 +17,26 @@ M.checkTimer = function (endTime)
 	end
 end
 
+M.convertTime = function (time)
+	local hours					= time % (SEC_PER_MIN * MIN_PER_HR)
+	local hourRemainder	= time - (hours * (SEC_PER_MIN * MIN_PER_HR))
+	local mins					= hourRemainder % SEC_PER_MIN
+	local secs					= hourRemainder - (mins * SEC_PER_MIN)
+
+	return { hours = hours, mins = mins, secs = secs}
+end
+
+M.displayTimer = function (endTime)
+	local timeRemaining			= M.checkTimer(endTime)
+	local hours, mins, secs = M.convertTime(timeRemaining)
+
+	local timeStr = string.format('%d:%d:%d', hours, mins, secs)
+	print(timeStr)
+end
+
+local endTime = M.startTimer(15)
+while (M.checkTimer(endTime) ~= 0) do
+	M.displayTimer(endTime)
+end
+
 return M
